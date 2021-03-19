ba55863c6abfe8ba1a8f15c85ac0976268862539
alphazhangyz <alphazhangyz@gmail.com>
2021-03-19 17:11:45 +0800

    量词

--- JavaScriptRegex/第一章/test.js
+++ JavaScriptRegex/第一章/test.js
@@ -6,9 +6,19 @@ const str1 = 'abc abbc abbbc abbbbc abbbbbc abbbbbbc'
 
 const reg2 = /a[123]b/g
 const str2 = 'a0b a1b a2b a3b a4b'
-testReg(reg2, str2)
+// testReg(reg2, str2)
 // [ 'a1b', 'a2b', 'a3b' ]
 
 function testReg (reg, str) {
   console.log(str.match(reg))
 }
+
+// 贪婪匹配
+const reg3 = /\d{2,5}/g
+
+//惰性匹配
+const reg4 = /\d{2,5}?/g
+const str3 = '123 1234 12345 123456'
+
+testReg(reg3, str3)
+testReg(reg4, str3)
